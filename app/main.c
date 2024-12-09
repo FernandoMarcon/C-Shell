ea892eb5d2cc602e05c09d7d4272b8cb9a734e46
FernandoMarcon <fmarcon13@gmail.com>
2024-12-09 14:38:55 -0300

    PATH

--- app/main.c
+++ app/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int starts_with(const char *str, const char *prefix) {
     return strncmp(str, prefix, strlen(prefix));
@@ -31,13 +33,52 @@ int is_builtin (char *input, char *args) {
     return found;
 }
 
+/* int file_exists ( const char *path ) { */
+/*     FILE *file = fopen(path, "r"); */
+/*     if (file != NULL) { */
+/*         fclose(file); */
+/*         return 1; */
+/*     } */
+/*     return 0; */
+/* } */
+
+/* int command_in_dir(const char *command, const char *dir) { */
+/*     char full_path[1024]; */
+/*     snprintf(full_path, sizeof(full_path), "%s%s", dir, command); */
+
+/*     return file_exists(full_path); */
+/* } */
+
+char* find_command_path(const char* command) {
+    char *path = getenv("PATH");
+    if (path == NULL) return NULL;
+
+    char *path_copy = strdup(path);
+    char *dir;
+    char full_path[1024];
+
+    dir = strtok(path_copy, ":");
+    while ( dir != NULL ) {
+        snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
+
+        if ( access(full_path, X_OK) == 0 ){
+            free(path_copy);
+            return strdup(full_path);
+        }
+
+        dir = strtok(NULL, ":");
+    }
+
+    free(path_copy);
+    return NULL;
+}
+
 int main() {
     while (1) {
 
       printf("$ ");
       fflush(stdout);
 
-      // Wait for user input
       char input[100];
       fgets(input, 100, stdin);
 
@@ -50,13 +91,14 @@ int main() {
           printf("%s\n", args);
 
       } else if (starts_with(input, "type") == 0) {
-          char *args = get_args(input, "type");
-          
-          if (is_builtin(input, args)) {
-              printf("%s is a shell builtin\n", args);
+          char *cmd = get_args(input, "type");
+          char *cmd_path = find_command_path(cmd);
+          if (cmd_path){
+              printf("%s is %s\n", cmd, cmd_path);
+              free(cmd_path);
           } else {
-              printf("%s: not found\n", args);
-          }          
+              printf("%s: not found\n", cmd);
+          }
 
       } else {
           printf("%s: command not found\n", input);
